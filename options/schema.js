d05ef6be6d3b17d686687a37d85619aa741ca7ac
SantiagoGutierrezHernandez <86387559+SantiagoGutierrezHernandez@users.noreply.github.com>
2022-05-24 23:19:43 -0300

    Desafío de usuarios y autenticación

---
+++ options/schema.js
@@ -0,0 +1,22 @@
+module.exports = {
+    products : {
+        title : String,
+        price: Number,
+        thumbnail: String
+    },
+    messages :{
+        author: {
+            id: String,
+            name: String,
+            surname : String,
+            age: Number,
+            nick: String,
+            avatar: String
+        },
+        msg: String
+    },
+    users:{
+        username:String,
+        password:String
+    }
+}
